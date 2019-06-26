97057a924d02addd45f707afeb0ceb2a9705ce12
Filipe Manuel Oliveira Fernandes de Sousa (CTS) <filipemanuelofs@gmail.com>
2019-06-26 13:46:22 -0300

    Implementação para compilação do Maven por projeto

--- was-deploy.py
+++ was-deploy.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 '''
-Script responsavel por realizar o undeploy e deploy do aplicativo no WAS.
+Versão: 2.0
+Script responsavel por realizar o undeploy e deploy do aplicativo no WAS, alem de compilar atraves do Maven.
 
-Execucao:
-	$ cd C:\\<CAMINHO_WAS>\\WebSphere\\AppServer\\bin
+Execução:
+	$ cd C:\<CAMINHO_WAS>\WebSphere\AppServer\bin
 	$ wsadmin.bat -language jython -f <CAMINHO_SCRIPT> -username wasadmin -password wasadmin
-	
+
+Autor: Filipe Manuel
 '''
 import time
 import os
@@ -15,46 +17,71 @@ import string
 import subprocess
 
 # Caminho do projeto
+# Ex: C:\\Desenvolvimento\\Projetos\\AppCorporativo\\
 PROJETO_BASE_LOCAL = '<CAMINHO_DO_PROJETO>'
 
 # Nome do aplicativo
-EAR_NOME = '<NOME_DO_APLICATIVO-EAR>'
+# Ex: app-corporativo
+APP_NOME = '<NOME_DO_APLICATIVO_EAR>'
 
 # Caminho do EAR
-EAR_LOCAL = PROJETO_BASE_LOCAL + '<CAMINHO_DA_PASTA_TARGET>'
+# Ex: \\AppCorporativoEAR\\target\\
+APP_LOCAL = PROJETO_BASE_LOCAL + '<CAMINHO_DA_PASTA_TARGET>'
+
+# Caminho dos projetos/pastas a serem compilados em ordem, portanto, os projetos de EAR e WAR devem ser os ultimos
+PROJETOS = [
+	'ModuloEJB',
+	'ModuloWAR',
+	'ModuloEAR'
+]
 
 '''
 Resposavel por compilar o projeto pelo Maven
 '''
 def compilar_maven():
+	projetos_compilados = []
 	if os.path.exists(PROJETO_BASE_LOCAL):
 		os.chdir(PROJETO_BASE_LOCAL)
 		print '###\n### Entrando na pasta {} ###\n###'.format(PROJETO_BASE_LOCAL)
-	
-		codigo = subprocess.call(["mvn", "clean", "install", "-DskipTests=true", "-o", "-Pwas"], shell=True)
 
+		for projeto in PROJETOS:
+			caminho_completo = PROJETO_BASE_LOCAL + projeto
+			if os.path.exists(caminho_completo):
+				os.chdir(caminho_completo)
+				print '###\n### Entrando na pasta {} ###\n###'.format(caminho_completo)
+
+				codigo = subprocess.call(["mvn", "clean", "install", "-DskipTests", "-Pwas", "-o"], shell=True)
+
+				if codigo == 0:
+					projetos_compilados.append(projeto)
+				else:
+					print '###\n### Projeto {} NAO compilado corretamente ###\n###'.format(projeto)
+					break
+			else:
+				print '###\n### Projeto NAO existente {} ###\n###'.format(projeto)
+		
 		if codigo == 0 or codigo is None:
-			print '###\n### Projeto compilado com sucesso ###\n###'
+			print '###\n### Projetos compilados com sucesso ###\n###'
 		else:
-			print '###\n### Não foi possível compilar o projeto ###\n###'
+			raise Exception('###\n### Não foi possível compilar o projeto ###\n###')
 
 '''
 Responsavel por realizar o deploy do aplicativo no WAS
 '''
 def deploy():
-	print '###\n### Realizando deploy do aplicativo: {} ###\n###'.format(EAR_NOME)
-	AdminApp.install(EAR_LOCAL + EAR_NOME + '.ear','[-node nd1 -cell cell01 -server server1]')
+	print '###\n### Realizando deploy do aplicativo: {} ###\n###'.format(APP_NOME)
+	AdminApp.install(APP_LOCAL + APP_NOME + '.ear','[-node nd1 -cell cell01 -server server1]')
 	AdminConfig.save()
 	appMan = AdminControl.queryNames('cell=cell01,node=nd1,type=ApplicationManager,process=server1,*')
-	AdminControl.invoke(appMan, 'startApplication', EAR_NOME)
+	AdminControl.invoke(appMan, 'startApplication', APP_NOME)
 	print '###\n### Deploy realizado com sucesso ###\n###'
 
 '''
 Responsavel por realizar o undeploy do aplicativo no WAS
 '''
 def undeploy(): 
-	print '###\n### Realizando undeploy do aplicativo: {} ###\n###'.format(EAR_NOME)
-	AdminApp.uninstall(EAR_NOME)
+	print '###\n### Realizando undeploy do aplicativo: {} ###\n###'.format(APP_NOME)
+	AdminApp.uninstall(APP_NOME)
 	AdminConfig.save()
 	print '###\n### Undeploy realizado com sucesso ###\n###'
 
@@ -63,9 +90,8 @@ Responsavel por recuperar o status do aplicativo no WAS
 '''
 def __app_status(aplicativo):
 	objeto = AdminControl.completeObjectName('type=Application,name=' + aplicativo + ',*')
-	if objeto == "":
-		status = 'Parado'
-	else:
+	
+	if objeto != "":
 		status = 'Rodando'
 	
 	return status
@@ -77,13 +103,21 @@ def app_status_info():
 	appsString = AdminApp.list()
 	print appsString
 	appList = string.split(appsString, '\r\n')
-	print '###\n### Status\t| Aplicativo ###\n###'
+	print '###\n### Status\t| Aplicativo ###'
 
 	for app in appList:
 		print '### ' + __app_status(app) + '\t| ' + app
 
 if __name__ == '__main__':
-	app_status_info()
-	undeploy()
-	compilar_maven()
-	deploy()
+	inicio = time.time()
+	
+	try:
+		app_status_info()
+		compilar_maven()
+		undeploy()
+		deploy()
+	except Exception as ex:
+		print 'Erro: {}'.format(ex)
+
+	minutos = ((time.time() - inicio) / 60)
+	print u'###\n### Tempo total: {} min ###'.format(minutos)
