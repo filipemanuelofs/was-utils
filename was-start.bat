d366896a3a2a9e4de11d3ebdd8dda1a89c30a570
Filipe M <filipemanuelofs@gmail.com>
2022-04-25 14:39:59 -0300

    Update was-start.bat

--- was-start.bat
+++ was-start.bat
@@ -1,8 +1,6 @@
 @ECHO OFF
-
 SET WAS_USER=""
 SET WAS_PWD=""
-
 ECHO [0] SAIR
 ECHO [1] INICIAR WAS
 ECHO [2] PARAR WAS
@@ -14,10 +12,8 @@ IF /I "%X%" == "2" GOTO 2
 IF /I "%X%" == "3" GOTO 3
 ECHO "OPCAO INVALIDA"
 GOTO END
-
 :0
 EXIT 0
-
 :1
 ECHO .
 ECHO :--------------------------
@@ -26,7 +22,6 @@ ECHO :--------------------------
 ECHO .
 %WAS_HOME%\bin\startServer.bat -profileName AppSrv01 server1 && START https://localhost:9043/ibm/console/unsecureLogon.jsp
 GOTO END
-
 :2
 ECHO .
 ECHO :--------------------------
@@ -35,7 +30,6 @@ ECHO :--------------------------
 ECHO .
 %WAS_HOME%\bin\stopServer.bat -profileName AppSrv01 server1 -username $WAS_USER -password $WAS_PWD
 GOTO END
-
 :3
 ECHO .
 ECHO :--------------------------
@@ -44,5 +38,4 @@ ECHO :--------------------------
 ECHO .
 %WAS_HOME%\bin\stopServer.bat -profileName AppSrv01 server1 -username wasadmin -password wasadmin && %WAS_HOME%\bin\startServer.bat -profileName AppSrv01 server1 && START https://localhost:9043/ibm/console/unsecureLogon.jsp
 :END
-
 PAUSE
