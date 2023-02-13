314f6ac317c7c39816e633ab31ac9a3099d8b1e4
Filipe M <filipemanuelofs@gmail.com>
2023-02-13 16:56:56 -0300

    Update was-start.bat

--- was-start.bat
+++ was-start.bat
@@ -1,41 +1,52 @@
 @ECHO OFF
+SET WAS_HOME=%WAS_HOME%
+SET WAS_PROFILE=AppSrv01
+SET WAS_SERVER=server1
 SET WAS_USER=""
 SET WAS_PWD=""
+
+:MENU
 ECHO [0] SAIR
 ECHO [1] INICIAR WAS
 ECHO [2] PARAR WAS
 ECHO [3] REINICIAR WAS
 SET /P X="O QUE DESEJA FAZER? "
-IF /I "%X%" == "0" GOTO 0
-IF /I "%X%" == "1" GOTO 1
-IF /I "%X%" == "2" GOTO 2
-IF /I "%X%" == "3" GOTO 3
-ECHO "OPCAO INVALIDA"
+IF /I "%X%" == "0" GOTO END
+IF /I "%X%" == "1" GOTO START
+IF /I "%X%" == "2" GOTO STOP
+IF /I "%X%" == "3" GOTO RESTART
+ECHO "OPÇÃO INVÁLIDA"
 GOTO END
-:0
-EXIT 0
-:1
+
+:START
 ECHO .
 ECHO :--------------------------
 ECHO ::::: INICIANDO SERVIDOR ::
 ECHO :--------------------------
 ECHO .
-%WAS_HOME%\bin\startServer.bat -profileName AppSrv01 server1 && START https://localhost:9043/ibm/console/unsecureLogon.jsp
+%WAS_HOME%\bin\startServer.bat -profileName %WAS_PROFILE% %WAS_SERVER%
+START https://localhost:9043/ibm/console/unsecureLogon.jsp
 GOTO END
-:2
+
+:STOP
 ECHO .
 ECHO :--------------------------
 ECHO ::::::: PARANDO SERVIDOR ::
 ECHO :--------------------------
 ECHO .
-%WAS_HOME%\bin\stopServer.bat -profileName AppSrv01 server1 -username $WAS_USER -password $WAS_PWD
+%WAS_HOME%\bin\stopServer.bat -profileName %WAS_PROFILE% %WAS_SERVER% -username %WAS_USER% -password %WAS_PWD%
 GOTO END
-:3
+
+:RESTART
 ECHO .
 ECHO :--------------------------
 ECHO ::: REINICIANDO SERVIDOR ::
 ECHO :--------------------------
 ECHO .
-%WAS_HOME%\bin\stopServer.bat -profileName AppSrv01 server1 -username wasadmin -password wasadmin && %WAS_HOME%\bin\startServer.bat -profileName AppSrv01 server1 && START https://localhost:9043/ibm/console/unsecureLogon.jsp
+%WAS_HOME%\bin\stopServer.bat -profileName %WAS_PROFILE% %WAS_SERVER% -username %WAS_USER% -password %WAS_PWD%
+%WAS_HOME%\bin\startServer.bat -profileName %WAS_PROFILE% %WAS_SERVER%
+START https://localhost:9043/ibm/console/unsecureLogon.jsp
+GOTO END
+
 :END
-PAUSE
+EXIT 0
